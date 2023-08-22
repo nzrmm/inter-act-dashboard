32e447bd622c3e736963891b6a9e796ee196f99c
muhammadgata <iamgata.me@gmail.com>
2023-08-23 00:34:40 +0700

    feat: add header layout

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -5,8 +5,21 @@ const Header = () => {
     <header
       className={cx("sticky top-0 bg-dark-01", " border-b border-b-[#4B5259]")}
     >
-      <div className={cx("h-[60px] flex justify-end items-center px-11")}>
-        Navbar
+      <div className={cx("h-[60px] flex justify-end items-center gap-3 px-11")}>
+        <div
+          className={cx(
+            "relative w-9 h-9 rounded-full",
+            " bg-[#B5B4B4] border border-white"
+          )}
+        >
+          <div
+            className={cx(
+              "absolute -right-1 top-1 w-2 h-2 rounded-full bg-[#5CCC5A]"
+            )}
+          ></div>
+        </div>
+
+        <p className={cx("font-medium text-sm")}>Hello, User</p>
       </div>
     </header>
   );
