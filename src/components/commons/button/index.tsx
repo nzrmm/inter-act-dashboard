8e4690a9840d220d6d80cf5ee9599807de2dc9ea
muhammadgata <iamgata.me@gmail.com>
2023-08-26 13:18:14 +0700

    feat: add button component

---
+++ src/components/commons/button/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import cx from "classnames";
+
+type IButton = React.PropsWithChildren<{
+  id: string;
+  size: "small";
+  variant: "primary";
+  customClassName?: string;
+  onClick?: () => void;
+}>;
+
+const Button = ({ id, children, size, variant, ...props }: IButton) => {
+  const sizeClass = {
+    small: "px-5 py-2 text-sm font-bold rounded-md",
+  }[size];
+
+  const variantClass = {
+    primary: "bg-[#0BB885] text-white hover:bg-[#0BB885]/90",
+  }[variant];
+
+  return (
+    <div>
+      <button
+        id={id}
+        className={cx(sizeClass, variantClass, props.customClassName)}
+        onClick={props.onClick}
+      >
+        {children}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
--- src/components/commons/index.ts
+++ src/components/commons/index.ts
@@ -1,6 +1,7 @@
 import Dot from "@/components/commons/dot";
+import Button from "@/components/commons/button";
 import Dropdown from "@/components/commons/dropdown";
 import Timeline from "@/components/commons/timeline";
 import TimelineItem from "@/components/commons/timeline-item";
 
-export { Dot, Dropdown, Timeline, TimelineItem };
+export { Dot, Button, Dropdown, Timeline, TimelineItem };
